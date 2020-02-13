e49b3901826d6ac04d530763c83c73ddb286bd84
Minh Nguyen <minhotmt@gmail.com>
2020-02-13 17:22:25 +0700

    get api image cat

---
+++ ReadYoutube/Views/ImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCell.swift
+//  ReadYoutube
+//
+//  Created by MinhNV-GD on 2/13/20.
+//  Copyright © 2020 MinhNV-GD. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+    
+    static let identifier = "ImageCell"
+    @IBOutlet weak var img: UIImageView!
+    
+}
