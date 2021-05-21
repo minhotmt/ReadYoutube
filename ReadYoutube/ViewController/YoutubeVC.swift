f6e99f3e38554b79deec3c60962a36c15cb6e40d
Minh Nguyen <minhnv@rikkeisoft.com>
2021-05-21 10:02:39 +0700

    add new viewcontroller

---
+++ ReadYoutube/ViewController/YoutubeVC.swift
@@ -0,0 +1,18 @@
+//
+//  YoutubeVC.swift
+//  ReadYoutube
+//
+//  Created by Minh Nguyen on 5/21/21.
+//  Copyright © 2021 MinhNV-GD. All rights reserved.
+//
+
+import UIKit
+
+class YoutubeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
