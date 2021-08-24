dbd747e871d9ce042decb64e2f78de4b6eaf1e08
Minh Nguyen <minhnv@rikkeisoft.com>
2021-08-24 15:07:38 +0700

    update set speed

--- ReadYoutube/ViewController/DetailVideoVC.swift
+++ ReadYoutube/ViewController/DetailVideoVC.swift
@@ -13,14 +13,22 @@ import AVKit
 class DetailVideoVC: UIViewController {
 
     @IBOutlet weak var viewPlayer: YTPlayerView!
-    @IBOutlet weak var v2: UIView!
-
+    @IBOutlet weak var sliderView: UISlider!
+    @IBOutlet weak var labelSpeed: UILabel!
+    
+    
     var idVideo: String = ""
+    var speedRate: Float = 1.2
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewPlayer.load(withVideoId: idVideo)
         viewPlayer.delegate = self
+    }
+    
+    @IBAction func sliderAction(_ sender: Any) {
+        labelSpeed.text = "\(Double(round(10*sliderView.value)/10))"
+        speedRate = sliderView.value
     }
 }
 
@@ -28,7 +36,7 @@ extension DetailVideoVC: YTPlayerViewDelegate {
     
     func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
         if state == .playing {
-            viewPlayer.setPlaybackRate(1.3)
+            viewPlayer.setPlaybackRate(speedRate)
         }
     }
     
